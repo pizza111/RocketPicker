44ec7d57bbab527f7625a4a7027115f02b8441d1
Piotr Woźniak <piotr.wozniak.career@gmail.com>
2023-07-29 09:25:56 +0200

    Initial Commit

---
+++ RocketPicker/RocketPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketPickerApp.swift
+//  RocketPicker
+//
+//  Created by Piotr Woźniak on 29/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct RocketPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
